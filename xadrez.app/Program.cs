d03bb57f394563e22fe0613547055fe91449476d
fabianoo17 <binnew19@gmail.com>
2021-09-05 13:31:15 -0300

    criando console aplication do xadrez

---
+++ xadrez.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
